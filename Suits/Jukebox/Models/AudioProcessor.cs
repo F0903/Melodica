de83d600f833e9dd76dd42f3d20cd46350188521
Frederik Popp <frepbh@gmail.com>
2020-02-21 12:35:22 +0100

    added 32 bit support for old branch

--- Suits/Jukebox/Models/AudioProcessor.cs
+++ Suits/Jukebox/Models/AudioProcessor.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 
+#nullable enable
+
 namespace Suits.Jukebox.Models
 {
     public sealed class AudioProcessor : IDisposable
@@ -16,8 +18,8 @@ namespace Suits.Jukebox.Models
             {
                 StartInfo = new ProcessStartInfo()
                 {
-                    FileName = "ffmpeg.exe",
-                    Arguments = $"-hide_banner -loglevel debug -vn {(format != null ? $"-f {format}" : string.Empty)} -i {$"\"{path}\"" ?? "pipe:0"} -f s16le -bufsize {bufferSize} -ab {bitrate} -ac 2 -ar 48000 -y pipe:1",
+                    FileName = Environment.Is64BitProcess ? "ffmpeg64.exe" : "ffmpeg32.exe",
+                    Arguments = $"-hide_banner -loglevel debug -vn {(format != null ? $"-f {format}" : string.Empty)} -i {$"\"{path}\"" ?? "pipe:0"} -f s16le -bufsize {bufferSize} -b:a {bitrate} -ac 2 -ar 48000 -y pipe:1",
                     UseShellExecute = false,
                     RedirectStandardError = false,
                     RedirectStandardInput = (inputAvailable = (path == null ? true : false)),
@@ -27,7 +29,6 @@ namespace Suits.Jukebox.Models
             };
 
             playerProcess.Start();
-            playerProcess.PriorityClass = ProcessPriorityClass.AboveNormal;
         }
 
         private readonly Process playerProcess;
