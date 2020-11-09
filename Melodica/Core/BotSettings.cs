39302003c0911daeefca925a60bfdbfaacd130e6
Frederik P. B. H <frepbh@gmail.com>
2020-11-09 17:25:06 +0100

    New token generated

--- Melodica/Core/BotSettings.cs
+++ Melodica/Core/BotSettings.cs
@@ -6,7 +6,7 @@ namespace Melodica.Core
 {
     public static class BotSettings
     {
-        public const string Token = "NTcxNDAwNTc4MDY0ODQyNzUy.XMNMQQ.8K9ovB1sbkExbYIK2wCI6OxzXSw";
+        public const string Token = "NTcxNDAwNTc4MDY0ODQyNzUy.XMNMQQ.K2HzgSELITBxXoukQ7L502p2bVY";
 
         public const string DefaultPrefix = "m.";
 
