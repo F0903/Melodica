8c93fdaf4a19fd345bb73362098a5ee110dddfc4
Frederik P. B. H <frepbh@gmail.com>
2020-11-15 18:30:54 +0100

    Refactored some methods to be static. Changed to names of some classes.

--- Melodica/Services/Logging/ColoredLogger.cs
+++ Melodica/Services/Logging/ColoredLogger.cs
@@ -1,28 +1,38 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Melodica.Services.Logging
 {
     public class ColoredLogger : IAsyncLogger
     {
-        public Task LogAsync(Discord.LogMessage msg)
+        static ColoredLogger()
         {
-            Console.ForegroundColor = msg.Severity switch
-            {
-                Discord.LogSeverity.Critical => ConsoleColor.Red,
-                Discord.LogSeverity.Error => ConsoleColor.DarkRed,
-                Discord.LogSeverity.Warning => ConsoleColor.Yellow,
-                Discord.LogSeverity.Info => ConsoleColor.White,
-                Discord.LogSeverity.Verbose => ConsoleColor.Cyan,
-                Discord.LogSeverity.Debug => ConsoleColor.Blue,
-                _ => ConsoleColor.White
-            };
-
-#if DEBUG
-            Console.WriteLine($"[{DateTime.Now} | {msg.Severity} | {msg.Source}] {msg.Message ?? msg.Exception.Message}");
-#else
+#if RELEASE
             Console.WriteLine("Release Mode");
 #endif
+        }
+
+        public Task LogAsync(Discord.LogMessage msg)
+        {
+            [Conditional("DEBUG")]
+            static void Log(Discord.LogMessage msg)
+            {
+                Console.ForegroundColor = msg.Severity switch
+                {
+                    Discord.LogSeverity.Critical => ConsoleColor.Red,
+                    Discord.LogSeverity.Error => ConsoleColor.DarkRed,
+                    Discord.LogSeverity.Warning => ConsoleColor.Yellow,
+                    Discord.LogSeverity.Info => ConsoleColor.White,
+                    Discord.LogSeverity.Verbose => ConsoleColor.Cyan,
+                    Discord.LogSeverity.Debug => ConsoleColor.Blue,
+                    _ => ConsoleColor.White
+                };
+
+                Console.WriteLine($"[{DateTime.Now} | {msg.Severity} | {msg.Source}] {msg.Message ?? msg.Exception.Message}");
+            }
+
+            Log(msg);
             return Task.CompletedTask;
         }
     }
