69733ec65fc42d3eb76236103c4cf1b7d4cd76f0
Frederik P. B. H <frepbh@gmail.com>
2021-01-02 08:01:40 +0100

    Added a pragma to disable a redundant warning

--- Melodica/Services/Serialization/BinarySerializer.cs
+++ Melodica/Services/Serialization/BinarySerializer.cs
@@ -8,14 +8,19 @@ namespace Melodica.Services.Serialization
 {
     public class BinarySerializer : IAsyncSerializer
     {
-        private static readonly BinaryFormatter bin = new BinaryFormatter(null, new StreamingContext(StreamingContextStates.File | StreamingContextStates.CrossProcess | StreamingContextStates.Persistence));
+        // Disable redundant warning. These files are only stored locally and contain no sensitive info.
+#pragma warning disable SYSLIB0011
 
-        //TODO: Convert this to use JSON to stop the screaming warning messages...
+        private static readonly BinaryFormatter bin = new BinaryFormatter(null, new StreamingContext(StreamingContextStates.File | StreamingContextStates.CrossProcess | StreamingContextStates.Persistence));
 
         public Task SerializeToFileAsync(string path, object toSerialize)
         {
             using var file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write);
+
+
             bin.Serialize(file, toSerialize);
+
+
             return Task.CompletedTask;
         }
 
@@ -24,5 +29,7 @@ namespace Melodica.Services.Serialization
             using var file = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
             return Task.FromResult((T)Convert.ChangeType(bin.Deserialize(file), typeof(T)));
         }
+
+#pragma warning restore SYSLIB0011
     }
 }
