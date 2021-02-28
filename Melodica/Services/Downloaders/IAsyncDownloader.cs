cdf3169f789aa16ce9a46340fb74a65d9541fb5f
Frederik P. B. H <frepbh@gmail.com>
2021-02-28 17:11:38 +0100

    started work on refactor

--- Melodica/Services/Downloaders/IAsyncDownloader.cs
+++ Melodica/Services/Downloaders/IAsyncDownloader.cs
@@ -10,22 +10,6 @@ namespace Melodica.Services.Downloaders
     {
         public static readonly IAsyncDownloader Default = new AsyncYoutubeDownloader();
 
-        protected Task<MediaType> EvaluateMediaTypeAsync(string url);
-
-        public bool IsUrlSupported(string url);
-
-        public Task<bool> VerifyUrlAsync(string url);
-
-        public Task<(MediaInfo playlist, IEnumerable<MediaInfo> videos)> DownloadPlaylistInfoAsync(string url);
-
-        public bool IsUrlPlaylistAsync(string url);
-
-        public Task<MediaInfo> GetMediaInfoAsync(string input);
-
-        public Task<PlayableMedia> DownloadAsync(string input);
-
-        public Task<PlayableMedia> DownloadAsync(MediaInfo input);
-
-        public Task<string> GetLivestreamAsync(string streamURL);
+        
     }
 }
