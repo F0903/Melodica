4e8ed503826a75e1289a6c4fb3663be3bccee69f
Frederik P <frepbh@gmail.com>
2022-11-21 23:26:51 +0100

    Soundcloud can now resolve a url.

--- Melodica/Services/Downloaders/SoundCloud/AsyncSoundcloudDownloader.cs
+++ Melodica/Services/Downloaders/SoundCloud/AsyncSoundcloudDownloader.cs
@@ -17,57 +17,21 @@ internal sealed partial class AsyncSoundcloudDownloader : IAsyncDownloader
     [GeneratedRegex(@"(((https)|(http)):\/\/)?soundcloud\.com\/{1}.+\/{1}.+", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline)]
     private static partial Regex SoundcloudUrlRegex();
 
-    private static readonly SearchClient search = new(new ClientInfo { ClientId = BotConfig.Secrets.SoundcloudClientID });
+    private static readonly SearchClient search = new(BotConfig.Secrets.SoundcloudClientID);
 
     private static readonly IMediaCache cache = new MediaFileCache("Soundcloud");
 
-    private static MediaInfo ToMediaInfo(Soundclouder.Media media)
+    private static MediaInfo ToMediaInfo(Track track)
     {
-        return new MediaInfo(media.ID.ToString())
+        return new MediaInfo(track.ID.ToString())
         {
-            Artist = media.Author,
-            Duration = TimeSpan.FromSeconds(Math.Round(media.Duration.TotalSeconds)), // Don't include all the milliseconds.
-            Title = media.Title,
+            Artist = track.Author,
+            Duration = TimeSpan.FromSeconds(Math.Round(track.Duration.TotalSeconds)), // Don't include all the milliseconds.
+            Title = track.Title,
             MediaType = MediaType.Video,
-            ImageUrl = media.ArtworkUrl,
-            Url = media.PermaLink,
+            ImageUrl = track.ArtworkUrl,
+            Url = track.PermaLink,
         };
-    }
-
-    private static ReadOnlySpan<char> ParseSearchQueryFromUrl(ReadOnlySpan<char> url)
-    {
-        //https://soundcloud.com/phatdix/haachama-club-banger-3000?in=caspar-friis/sets/clubn-bagner&si=2233d96e6c3240eea190fbd28dd03e90&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing
-        var numOfSlashes = 4;
-        if (!(url.StartsWith("https") || url.StartsWith("http")))
-        {
-            numOfSlashes -= 2;
-        }
-
-        var slashIndex = 0;
-        var encounters = 0;
-        for (int i = 0; i < url.Length; i++)
-        {
-            var ch = url[i];
-            if (ch != '/') continue;
-            encounters++;
-            if (encounters >= numOfSlashes)
-            {
-                slashIndex = i;
-                break;
-            }
-        }
-
-        var endIndex = url.Length;
-        for (int i = slashIndex; i < url.Length; i++)
-        {
-            var ch = url[i];
-            if (ch != '?') continue;
-            endIndex = i;
-            break;
-        }
-
-        var str = url.Slice(slashIndex + 1, endIndex - slashIndex - 1);
-        return str;
     }
 
     public Task<MediaCollection> DownloadAsync(MediaInfo info)
@@ -84,7 +48,7 @@ internal sealed partial class AsyncSoundcloudDownloader : IAsyncDownloader
             var converted = new MemoryStream();
             await output.CopyToAsync(converted);
             return new DataPair(converted, "s16le");
-        }, cache); 
+        }, cache);
         var lazyMedia = new LazyMedia(media);
         return Task.FromResult(new MediaCollection(media));
     }
@@ -92,15 +56,26 @@ internal sealed partial class AsyncSoundcloudDownloader : IAsyncDownloader
     public async Task<MediaInfo> GetInfoAsync(ReadOnlyMemory<char> query)
     {
         var queryString = query.Span.ToString();
+        Track? track = null;
         if (query.IsUrl())
         {
-            queryString = ParseSearchQueryFromUrl(queryString).ToString();
+            var result = await search.ResolveAsync(queryString);
+            if (result is TrackResolveResult trackResult)
+            {
+                track = trackResult.Track; 
+            }
         }
+        else
+        {
+            var result = await search.SearchAsync(queryString);
+            track = result.ReturnedMedia.First();
+        }
+        if (track == null)
+            throw new Exception("Track was not available!");
 
-        var result = await search.SearchAsync(queryString);
-        var rawMedia = result.ReturnedMedia.First();
-        var media = ToMediaInfo(rawMedia);
-        media.Passthrough = await rawMedia.GetStreamURLAsync(); //Kinda smelly.
+
+        var media = ToMediaInfo(track);
+        media.Passthrough = await track.GetStreamURLAsync(); //Kinda smelly.
         return media;
     }
 
